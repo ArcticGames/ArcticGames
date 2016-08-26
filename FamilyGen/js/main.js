94ce8baeff4be3a8de7b710fdfbebc4d6ad1c429
leoworm <leo.charles.p@gmail.com>
2016-08-26 12:42:21 +0200

    Added CSV to Array
    Uses the Papa-Parse library to convert CSV files to Array

--- FamilyGen/js/main.js
+++ FamilyGen/js/main.js
@@ -1,4 +1,16 @@
+//IDEA: Remove jquery.csv.js?
 $(document).ready(function() {
   console.log('loading succesful');
-  alert(chance.name());
+  var csvfile = "http://192.168.178.24:8080/js/materials/maleNames.csv";
+
+   Papa.parse(csvfile, {
+    download: true,
+    complete: function(output) {
+      console.log(output.data);
+      var thing = output.data
+      alert(thing[6]);
+    }
+  })
+
+
 });
