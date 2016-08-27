3b3826f10cf158e2e5152f9c17deb8b95b1c7010
leoworm <leo.charles.p@gmail.com>
2016-08-27 11:01:25 +0200

    Added Clock-Div

--- JgBtl.242/js/main.js
+++ JgBtl.242/js/main.js
@@ -1,18 +1,12 @@
 $(document).ready(function() {
-  var nextEvent = new Date(2016, 7, 27)
-  var now = new Date()
-  var diff = (nextEvent.getTime()/1000) - (now.getTime()/1000);
-  var clock = $('.clock').FlipClock(diff, {
-		clockFace: 'DailyCounter',
-		countdown: true,
-		showSeconds: false
-	});
-
-    var time  = clock.getCountdown();
-    alert(time);
+  $('.clock').TimeCircles(){ time: {
+    Days: { show: "false" },
+    Hours: { show: "false" },
+    Minutes: { show: "false" },
+    Seconds: { show: "false" }
+}}
 
 
 
 
-
-})
+});
