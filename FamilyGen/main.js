56b996b797c08f00a15ebbad620e0175eb685d1b
leoworm <leo.charles.p@gmail.com>
2016-12-06 18:49:30 +0100

    People can die now
    Also added a few new commands

--- FamilyGen/main.js
+++ FamilyGen/main.js
@@ -12,7 +12,7 @@ var Surnames = './materials/surnames.csv', surnames = fs.readFileSync(Surnames, 
 var Values = './materials/values.csv', valuesFile = fs.readFileSync(Values, {encoding:'binary'});
 var rl = readline.createInterface(process.stdin, process.stdout);
 
-//Settings, e.g. likelihood
+//Settings, e.g. likelihood; To be replaced with csv files
 var isBorn = 35;
 var tickSpeed = 3000; //Given in Miliseconds
 var eras = {medieval: 0, renaissance: 500, industrial: 800, modern: 1000};
@@ -22,6 +22,7 @@ var people = {};
 var year = 0;
 
 rl.setPrompt('Year is '+year+'.  > ');
+
 //All of the functions are stored here. They should be pretty self-explanatory
 function newEntry(name, gender, fname, lastName, birthyear) {
   var foo = chance.natural({min:1, max:999999});
@@ -41,6 +42,55 @@ function newEntry(name, gender, fname, lastName, birthyear) {
   };
   //DEBUG: console_out('Created Entry: ' + name + ' with ID: ' + id.green + '. Gender is: ' + gender.red);
 };
+function between(x, min, max) {
+  return x >= min && x <= max;
+};
+//This function checks who will die this year by using values from a CSV file.
+function unleashGrimReaper(){
+  list = Object.keys(people);
+  list.forEach(function(element){
+    var age = people[element].age;
+    if(people[element].dead!==true){
+      if(between(age, values[8][1], values[9][1])) {
+        var death = chance.bool({likelihood: values[1][1]});
+        if(death==true){
+          people[element].dead = true;
+          people[element].yearOfDeath = year;
+          console_out(people[element].name +' '+people[element].lastName+' has died at the age of '+people[element].age);
+        }
+      }else if (between(age, values[8][2], values[9][2])) {
+        var death = chance.bool({likelihood: values[2][1]});
+        if(death==true){
+          people[element].dead = true;
+          people[element].yearOfDeath = year;
+          console_out(people[element].name +' '+people[element].lastName+' has died at the age of '+people[element].age);
+        }
+      }else if (between(age, values[8][3], values[9][3])) {
+        var death = chance.bool({likelihood: values[3][1]});
+        if(death==true){
+          people[element].dead = true;
+          people[element].yearOfDeath = year;
+          console_out(people[element].name +' '+people[element].lastName+' has died at the age of '+people[element].age);
+        }
+      }else if (between(age, values[8][4], values[9][4])) {
+        var death = chance.bool({likelihood: values[4][1]});
+        if(death==true){
+          people[element].dead = true;
+          people[element].yearOfDeath = year;
+          console_out(people[element].name +' '+people[element].lastName+' has died at the age of '+people[element].age);
+        }
+      }else if (between(age, values[8][5], values[9][5])) {
+        var death = chance.bool({likelihood: values[4][1]});
+        if(death==true){
+          people[element].dead = true;
+          people[element].yearOfDeath = year;
+          console_out(people[element].name +' '+people[element].lastName+' has died at the age of '+people[element].age);
+        }
+      }
+    }
+  })
+};
+
 //This function allows for the bottom line of the terminal to stay free of output.
 //https://code.tutsplus.com/tutorials/real-time-chat-with-nodejs-readline-socketio--cms-20953
 function console_out(msg) {
@@ -51,11 +101,13 @@ function console_out(msg) {
   };
     rl.prompt(true);
 };
-//TODO: Make them be able to die using a new attribute which is checked.
+//TODO: Add a new message for death at birth
 function increaseAge() {
   list = Object.keys(people)
   list.forEach(function(element){
-    people[element].age = people[element].age + 1;
+    if(people[element].dead!==true){
+      people[element].age = people[element].age + 1;
+    }
   });
 };
 
@@ -78,6 +130,7 @@ Papa.parse(surnames, {
 Papa.parse(valuesFile, {
   complete: function(output) {
     values = output.data;
+    //console.log(values[8][1]);
   }
 });
 
@@ -92,20 +145,24 @@ function loop() {
     var mName = mOutName[maleNameNum].toString();
     var fName = fOutName[femaleNameNum].toString();
     var sName = sOutName[surnameNum].toString();
+    var cleanMaleName = mName.toLowerCase().split(' ')[0]+'_';
+    var cleanFemaleName = fName.toLowerCase().split(' ')[0]+'_';
+    var CleanSurname = sName.toLowerCase(), cleanSurname = CleanSurname.replace(/ /g, '_');
     var born = chance.bool({likelihood: isBorn});
     //Here it is checked if a child is born this year, and if so which gender it is.
     if(born==true){
       if(gender=='Male'){
-        newEntry(mName.toLowerCase() + '_' + sName.toLowerCase(), gender, mName, sName, year);
+        newEntry(cleanMaleName + cleanSurname, gender, mName, sName, year);
         console_out(mName+' '+sName+" was born! It's a boy!");
       }else if (gender=='Female') {
-        newEntry(fName.toLowerCase() + '_' + sName.toLowerCase(), gender, fName, sName, year);
+        newEntry(cleanFemaleName + cleanSurname, gender, fName, sName, year);
         console_out(fName+' '+sName+" was born! It's a girl!");
       };
     };
     //All values are increased here and the loop is restarted.
     rl.setPrompt('Year is '+year+'.  > ');
     console_out();
+    unleashGrimReaper();
     year++
     increaseAge();
     loop();
@@ -118,14 +175,43 @@ loop();
 //This is the event handler for pressing enter. All commands are to be stored here.
 rl.on('line', function (line) {
   var input = line.split(' ');
+  var list = Object.keys(people);
   if(input[0]=='info'){
     var dataSource = input[1]
     console.log(dataSource);
     console.log(people[dataSource]);
-    console_out(people[dataSource].attributes[0].test)
   }else if (input[0]=='exit') {
     console.log('Exiting...');
     process.exit();
+  }else if (input[0]=='people') {
+    console_out('\n');
+    console_out('-----------------------------------------------'.green);
+    if(people.length==0){
+      console_out('There are currently no people (for whatever reason)');
+    }else{
+      if(input[1]=='alive'){
+        var i = 0;
+        list.forEach(function(element){
+          if(people[element].dead!==true){
+            console_out(element+' is currently ' + people[element].age.toString().green + ' years old.')
+            i++
+          };
+        });
+        if(i==0) {
+          console_out('There is no one currently alive');
+        }
+      }else{
+        list.forEach(function(element){
+          if(people[element].dead==true){
+            console_out(element + ', currently ' + 'dead.'.red);
+          }else{
+            console_out(element + ', currently ' + 'alive.'.green);
+          };
+        });
+      };
+    }
+    console_out('-----------------------------------------------'.green);
+    console_out('\n');
   };
   rl.prompt(true);
 });
